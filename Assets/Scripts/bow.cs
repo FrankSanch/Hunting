2f838ad7060ffbf04951f90088d1d6cfab090974
FrankSanch <fsanchez2@hotmail.fr>
2019-01-29 22:14:33 -0500

    no message

---
+++ Assets/Scripts/bow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bow : MonoBehaviour
+{
+    private GameObject bowMiddle;
+    private GameObject bowTop;
+
+   
+    void Start()
+    {
+        bowMiddle = GameObject.CreatePrimitive(PrimitiveType.Cylinder);   //Initialiser un cylindre qui servira de manche pour l'arc
+        bowMiddle.transform.SetParent(this.transform, false);
+        bowMiddle.transform.localPosition = new Vector3(0, 0, 0);
+        bowMiddle.transform.localScale = new Vector3((float)0.03, (float)0.3, (float)0.02);
+
+        bowTop = GameObject.CreatePrimitive(PrimitiveType.Cylinder);   //Initialiser un cylindre qui servira de manche pour l'arc
+        bowTop.transform.SetParent(this.transform, false);
+        bowTop.transform.localPosition = new Vector3(0, 0, 0);
+        bowTop.transform.localScale = new Vector3((float)0.03, (float)0.3, (float)0.02);
+        bowTop.transform.localRotation = Quaternion.AngleAxis(30, Vector3.left);
+
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
