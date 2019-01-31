8cd409ed0334bce6e9a27219d4be74eb23f675db
FrankSanch <fsanchez2@hotmail.fr>
2019-01-31 00:02:21 -0500

    Bow Prototype + Camera angle

---
+++ Assets/Scripts/PLayerLookAt.cs
@@ -0,0 +1,28 @@
+﻿//Sert a faire que la camera suit le curseur
+ 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayerLookAt : MonoBehaviour
+{
+    public bool showCursor;   //si on voit le curseur ou pas;
+    public float sensitivity; //Sensibilite de la souris
+    
+    void Start()
+    {
+        if (showCursor == false)
+            Cursor.visible = false; //Cacher le cursor
+
+        //this.transform.localEulerAngles.y -= 153.372f;
+    }
+
+    
+    void Update()
+    {
+        float newRotationY = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivity;
+        float newRotationX = transform.localEulerAngles.x + Input.GetAxis("Mouse Y") * sensitivity;
+
+        this.transform.localEulerAngles = new Vector3(newRotationX, newRotationY, 0);
+    }
+}
--- Assets/Scripts/bow.cs
+++ Assets/Scripts/bow.cs
@@ -4,26 +4,40 @@ using UnityEngine;
 
 public class bow : MonoBehaviour
 {
-    private GameObject bowMiddle;
-    private GameObject bowTop;
+    private GameObject bowMiddle;//milieu de l'arc
+    private GameObject bowTop;//top de l'arc
+    private GameObject bowBot; //bas de l'arc
+
 
    
     void Start()
     {
-        bowMiddle = GameObject.CreatePrimitive(PrimitiveType.Cylinder);   //Initialiser un cylindre qui servira de manche pour l'arc
+        bowMiddle = GameObject.CreatePrimitive(PrimitiveType.Cylinder);   //Initialiser un cylindre qui servira de manche pour le milieu de l'arc.
         bowMiddle.transform.SetParent(this.transform, false);
-        bowMiddle.transform.localPosition = new Vector3(0, 0, 0);
-        bowMiddle.transform.localScale = new Vector3((float)0.03, (float)0.3, (float)0.02);
+        bowMiddle.transform.localPosition = new Vector3(0, (float)0.5, 0);
+        bowMiddle.transform.localScale = new Vector3((float)0.03, (float)0.12, (float)0.02);
 
-        bowTop = GameObject.CreatePrimitive(PrimitiveType.Cylinder);   //Initialiser un cylindre qui servira de manche pour l'arc
-        bowTop.transform.SetParent(this.transform, false);
-        bowTop.transform.localPosition = new Vector3(0, 0, 0);
+        bowMiddle.GetComponent<CapsuleCollider>().isTrigger = true; // sets istrigger to avoid collision with the ground
+
+        bowTop = GameObject.CreatePrimitive(PrimitiveType.Cylinder);   //Initialiser un cylindre qui sera la partie superieur du manche de l'arc
+        bowTop.transform.SetParent(this.transform, false);  
         bowTop.transform.localScale = new Vector3((float)0.03, (float)0.3, (float)0.02);
         bowTop.transform.localRotation = Quaternion.AngleAxis(30, Vector3.left);
+        bowTop.transform.localPosition = new Vector3(0, (float)0.875, (float)-0.15);
+
+        bowTop.GetComponent<CapsuleCollider>().isTrigger = true;
+
+        bowBot = GameObject.CreatePrimitive(PrimitiveType.Cylinder);   //Initialiser un cylindre qui sera la partie inferieur du manche de l'arc
+        bowBot.transform.SetParent(this.transform, false);
+        bowBot.transform.localScale = new Vector3((float)0.03, (float)0.3, (float)0.02);
+        bowBot.transform.localRotation = Quaternion.AngleAxis(30, Vector3.right);
+        bowBot.transform.localPosition = new Vector3(0, (float)0.125, (float)-0.15);
+
+        bowBot.GetComponent<CapsuleCollider>().isTrigger = true;
 
     }
 
-   
+
     void Update()
     {
         
