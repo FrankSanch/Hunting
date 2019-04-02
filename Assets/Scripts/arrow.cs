5345288c49424234e8d3a9eb67e054c5a3d21971
UnBleuetSauvage <jul_hm96@hotmail.com>
2019-04-02 11:21:01 -0400

    UI update

--- Assets/Scripts/arrow.cs
+++ Assets/Scripts/arrow.cs
@@ -17,8 +17,8 @@ public class arrow : MonoBehaviour
     {
         Gamedataboy = GameObject.Find("Gamedata");
         body = GetComponent<Rigidbody>();
-        transform.rotation = Quaternion.LookRotation(body.velocity);
-        
+        transform.rotation = Quaternion.LookRotation(body.velocity); 
+
     }
     void FixedUpdate()
     {
